3e25c1ba7cb6e845bcadf24c63dc5c09716e1bd1
xingcheng88 <502430648@qq.com>
2019-06-19 15:29:26 +0800

    Lab-5: Add CRUD to REST

---
+++ RestServiceClientLab5/src/main/java/com/xingcheng/restc/App.java
@@ -0,0 +1,14 @@
+package com.xingcheng.restc;
+
+import com.sun.jersey.api.client.Client;
+/**
+ * @author XingCheng
+ * @date 16.06.2019
+ */
+public class App {
+    public static void main(String[] args) {
+        Client client = Client.create();
+        ActionMenu menu = new ActionMenu();
+        menu.start(client);
+    }
+}
