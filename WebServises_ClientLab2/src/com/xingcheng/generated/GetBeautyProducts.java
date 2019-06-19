048a287972eef72da0a70c5f5a6f334a19741936
xingcheng88 <502430648@qq.com>
2019-06-19 15:26:00 +0800

    Lab-2: Implement CRUD for standalone service

---
+++ WebServises_ClientLab2/src/com/xingcheng/generated/GetBeautyProducts.java
@@ -0,0 +1,34 @@
+package com.xingcheng.generated;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * @author XingCheng
+ * @date 13.06.2019
+ *       <p>
+ *       Java class for getBeautyProducts complex type.
+ * 
+ *       <p>
+ *       The following schema fragment specifies the expected content contained
+ *       within this class.
+ * 
+ *       <pre>
+ * &lt;complexType name="getBeautyProducts">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ *       </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getBeautyProducts")
+public class GetBeautyProducts {
+
+}
---
+++ WebServises_ClientLab2/src/com/xingcheng/generated/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://lab1.xingcheng.com/")
+package com.xingcheng.generated;
